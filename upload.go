7efb75fe8c97ca1a60942212b08859eb23607152
James Hunt <james@niftylogic.com>
2020-05-30 11:43:31 -0400

    Parallel I/O upload writers

--- upload.go
+++ upload.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type xmlpart struct {
@@ -59,26 +60,32 @@ func (c *Client) NewUpload(path string, headers *http.Header) (*Upload, error) {
 	}, nil
 }
 
-func (u *Upload) Write(b []byte) error {
-	if u.n == 0 {
-		u.n = 1
-	}
-	if u.n > 10000 {
+func (u *Upload) nextPart() int {
+	u.parts = append(u.parts, xmlpart{})
+	u.n = u.n + 1
+	return u.n
+}
+
+func (u *Upload) writePart(b []byte, n int) error {
+	if n > 10000 {
 		return fmt.Errorf("S3 limits the number of multipart upload segments to 10k")
 	}
 
-	res, err := u.c.put(fmt.Sprintf("%s?partNumber=%d&uploadId=%s", u.path, u.n, u.id), b, nil)
+	res, err := u.c.put(fmt.Sprintf("%s?partNumber=%d&uploadId=%s", u.path, n, u.id), b, nil)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 
-	u.parts = append(u.parts, xmlpart{
-		PartNumber: u.n,
+	u.parts[n - 1] = xmlpart{
+		PartNumber: n,
 		ETag:       res.Header.Get("ETag"),
-	})
-	u.n++
+	}
 	return nil
+}
+
+func (u *Upload) Write(b []byte) error {
+	return u.writePart(b, u.nextPart())
 }
 
 func (u *Upload) Done() error {
@@ -104,14 +111,47 @@ func (u *Upload) Done() error {
 	return nil
 }
 
-func (u *Upload) Stream(in io.Reader, block int) (int64, error) {
+func (u *Upload) ParallelStream(in io.Reader, block int, threads int) (int64, error) {
 	if block < 5*1024*1024 {
 		return 0, fmt.Errorf("S3 requires block sizes of 5MB or higher")
 	}
 
+	type chunk struct {
+		n int
+		block []byte
+	}
+
+	var wg sync.WaitGroup
+	chunks := make(chan chunk, 0)
+	errors := make(chan error, threads)
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+		//idx := i
+		go func() {
+			defer wg.Done()
+			//fmt.Printf("io/%d: waiting for first chunk...\n", idx)
+			for chunk := range chunks {
+				//fmt.Printf("io/%d: writing %d bytes in chunk %d via writePart...\n", idx, len(chunk.block), chunk.n)
+				err := u.writePart(chunk.block, chunk.n)
+				if err != nil {
+					errors <- err
+					return
+				}
+				//fmt.Printf("io/%d: waiting for next chunk...\n", idx)
+			}
+		}()
+	}
+
 	var total int64
-	buf := make([]byte, block)
+	defer func() {
+		//fmt.Printf("... closing chunks channel\n")
+		close(chunks)
+		//fmt.Printf("... waiting for upload goroutines to exit")
+		wg.Wait()
+	}()
+
 	for {
+		buf := make([]byte, block)
 		nread, err := io.ReadAtLeast(in, buf, block)
 		if err != nil && err != io.ErrUnexpectedEOF {
 			if err == io.EOF {
@@ -120,14 +160,24 @@ func (u *Upload) Stream(in io.Reader, block int) (int64, error) {
 			return total, err
 		}
 
-		werr := u.Write(buf[0:nread])
-		if werr != nil {
-			return total, werr
+		chunks <- chunk{
+			n: u.nextPart(),
+			block: buf[0:nread],
 		}
-		total += int64(nread)
 
+		total += int64(nread)
 		if err == io.ErrUnexpectedEOF {
 			return total, nil
 		}
+
+		select {
+		default:
+		case err := <-errors:
+			return total, err
+		}
 	}
+}
+
+func (u *Upload) Stream(in io.Reader, block int) (int64, error) {
+	return u.ParallelStream(in, block, 1)
 }
